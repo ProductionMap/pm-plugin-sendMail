31833f9676946d81c7f2278dddd70818d4f8522f
kadosh1000 <kadosh10001@gmail.com>
2018-11-25 13:39:09 +0200

    * fix plugin structure

--- src/app.js
+++ src/app.js
@@ -19,25 +19,6 @@ function sendMail(action) {
 }
 
 
-var functions = {
+module.exports = {
     mail: sendMail
 };
-
-function main(argv) {
-    if (argv.length < 3) {
-        console.log('{err: "not enough parameters"}');
-        // Invalid Argument - Either an unknown option was specified, or an option requiring a value was provided without a value.
-        process.exit(9);
-    }
-    var action = JSON.parse(argv[2]);
-    functions[action.method.name](action).then(function (res) {
-        console.log(res);
-        process.exit(0); // Success
-    }).catch(function (error) {
-        console.log("Error sending mail", error);
-        // Uncaught Fatal Exception - There was an uncaught exception, and it was not handled by a domain or an 'uncaughtException' event handler.
-        process.exit(1);
-    });
-}
-
-main(process.argv);
